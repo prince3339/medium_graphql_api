1eda72c9aba629a24a080d2870af4f4896be94a7
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2019-12-04 16:46:03 +0600

    db setup

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :medium_graphql_api, MediumGraphqlApi.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "sabbir",
+  password: "sabbir",
   database: "medium_graphql_api_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
