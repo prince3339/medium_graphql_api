b640aa1b562f2c45a376d2ea9dc7608a3308800f
Sabbir Ahmed Siddiquee <prince3339@ymail.com>
2019-12-27 18:24:34 +0600

    add userTypes to schema

--- lib/medium_graphql_api_web/schema.ex
+++ lib/medium_graphql_api_web/schema.ex
@@ -1,7 +1,8 @@
-defmodule MediumGraphqlApiWeb.Schema
+defmodule MediumGraphqlApiWeb.Schema do
   use Absinthe.Schema
   
   # import Types
+  import_types(MediumGraphqlApiWeb.Schema.Types)
 
   query do
 
